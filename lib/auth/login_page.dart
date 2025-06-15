60186c29ed14dd18120af9115a0fbff85b0f27eb
mohamed mohamed <mof4943@gmail.com>
2025-06-15 20:03:23 +0300

    make appbar screens for two auth

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -5,6 +5,9 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+    appBar: AppBar(
+      title: const Text('login screen'),),
+    );
   }
 }
--- lib/auth/signup_page.dart
+++ lib/auth/signup_page.dart
@@ -5,6 +5,9 @@ class SignupPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('signup screen'),),
+    );
   }
 }
